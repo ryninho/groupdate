ec8d126655c793b7bc5f8a3655e7fbcbbac597a5
Eric R <ryninho@users.noreply.github.com>
2015-10-02 15:37:12 -0700

    redshift didn't like having a field named timestamp

--- groupdate_for_redshift.sql
+++ groupdate_for_redshift.sql
@@ -1,17 +1,17 @@
 -- day w/ time zone
 
 
-CREATE OR REPLACE FUNCTION gd_day(timestamp TIMESTAMP, time_zone_name TEXT)
+CREATE OR REPLACE FUNCTION gd_day(timestamp_col TIMESTAMP, time_zone_name TEXT)
     RETURNS TIMESTAMP
 STABLE
 AS $$
     from datetime import datetime
     import pytz
     from pytz import timezone
-    if timestamp.tzinfo is None:
-      timestamp = timestamp.replace(tzinfo=pytz.utc)
+    if timestamp_col.tzinfo is None:
+      timestamp_col = timestamp.replace(tzinfo=pytz.utc)
     local_tz = timezone(time_zone_name)
-    local_time = timestamp.astimezone(local_tz)
+    local_time = timestamp_col.astimezone(local_tz)
     local_date = local_time.replace(hour=0, minute=0, second=0, microsecond=0)
     return local_date.replace(tzinfo=None)
 $$ LANGUAGE plpythonu;
